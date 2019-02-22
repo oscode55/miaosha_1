6b7fbd89e3a46a02cd39bbb8d2888c1a11b39b1c
lijiecheng <1016030044@qq.com>
2019-02-23 02:41:08 +0800

    第二章 分布式session

--- src/main/java/com/lijiecheng/miaosha/dao/UserDao.java
+++ src/main/java/com/lijiecheng/miaosha/dao/UserDao.java
@@ -14,8 +14,8 @@ import org.apache.ibatis.annotations.Select;
 public interface UserDao {
 
     @Select("select * from user where id=#{id}")
-    public User getById(@Param("id") int id);
+    User getById(@Param("id") int id);
 
     @Insert("insert into user(id,name) values(#{id},#{name})")
-    public int insert(User user);
+    int insert(User user);
 }

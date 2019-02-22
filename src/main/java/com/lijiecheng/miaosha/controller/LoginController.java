35c178156bd4e3517c10d81963f7ab30c8f39ccb
lijiecheng <1016030044@qq.com>
2019-02-23 02:52:45 +0800

    Revert "Revert "第二章 两次MD5处理""
    This reverts commit 473d506

---
+++ src/main/java/com/lijiecheng/miaosha/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.lijiecheng.miaosha.controller;
+
+import com.lijiecheng.miaosha.domain.MiaoshaUser;
+import com.lijiecheng.miaosha.result.CodeMsg;
+import com.lijiecheng.miaosha.result.Result;
+import com.lijiecheng.miaosha.service.MiaoshaUserService;
+import com.lijiecheng.miaosha.util.ValidatorUtil;
+import com.lijiecheng.miaosha.vo.LoginVo;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import javax.validation.Validator;
+
+/**
+ * @Author: myname
+ * @Date: 2019/2/19 2:28
+ */
+@Controller
+@RequestMapping("/login")
+@Slf4j
+public class LoginController {
+
+    @Autowired
+    private MiaoshaUserService userService;
+
+    @RequestMapping("/to_login")
+    public String toLogin() {
+        return "login";
+    }
+
+    @RequestMapping("/do_login")
+    @ResponseBody
+    public Result<Boolean> doLogin(HttpServletResponse response,@Validated LoginVo loginVo ) {
+        log.info(loginVo.toString());
+        //登录
+       userService.login(response,loginVo);
+       return Result.success(true);
+    }
+
+
+
+
+}

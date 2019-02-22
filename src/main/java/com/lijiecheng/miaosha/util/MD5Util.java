473d5060cedb8817c47b9b8a32d090b6a47ce0eb
lijiecheng <1016030044@qq.com>
2019-02-23 02:43:27 +0800

    Revert "第二章 两次MD5处理"
    This reverts commit 731ab33

--- src/main/java/com/lijiecheng/miaosha/util/MD5Util.java
+++ src/main/java/com/lijiecheng/miaosha/util/MD5Util.java
@@ -13,19 +13,12 @@ public class MD5Util {
 
     private static final String salt = "1a2b3c4d";
 
-    public static String inputPassToFormPass(String inputPass){
+    public static String inputPassFormPass(String inputPass){
         String str = ""+salt.charAt(0)+salt.charAt(2)+inputPass+salt.charAt(5)+salt.charAt(4);
         return md5(str);
     }
 
-    public static String formPassToDBPass(String formPass,String salt){
-        String str = ""+salt.charAt(0)+salt.charAt(2)+formPass+salt.charAt(5)+salt.charAt(4);
-        return md5(str);
-    }
-
-    public static String inputPassToDBPass(String input,String saltDB){
-        String formPass = inputPassToFormPass(input);
-        String dbPass = formPassToDBPass(formPass,saltDB);
-        return dbPass;
+    public static void main(String[] args) {
+        System.out.println(inputPassFormPass("123456"));
     }
 }

347831565a56728d48603a5cc530937a4766ade3
lijiecheng <1016030044@qq.com>
2019-03-02 01:12:36 +0800

    第七章 防止消息重发

--- src/main/java/com/lijiecheng/miaosha/rabbitmq/MQReceiver.java
+++ src/main/java/com/lijiecheng/miaosha/rabbitmq/MQReceiver.java
@@ -7,48 +7,60 @@ import com.lijiecheng.miaosha.service.GoodsService;
 import com.lijiecheng.miaosha.service.MiaoshaService;
 import com.lijiecheng.miaosha.service.OrderService;
 import com.lijiecheng.miaosha.vo.GoodsVo;
+import com.rabbitmq.client.AMQP;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+import sun.rmi.transport.Channel;
+
+import java.io.IOException;
+
 
 @Service
 @Slf4j
 public class MQReceiver {
-		
-		@Autowired
-		RedisService redisService;
-		
-		@Autowired
-		GoodsService goodsService;
-		
-		@Autowired
-		OrderService orderService;
-		
-		@Autowired
-		MiaoshaService miaoshaService;
 
-		@RabbitListener(queues=MQConfig.MIAOSHA_QUEUE)
-		public void receive(String message) {
-			log.info("receive message:"+message);
+    @Autowired
+    RedisService redisService;
 
-			MiaoshaMessage mm  = RedisService.stringToBean(message, MiaoshaMessage.class);
-			MiaoshaUser user = mm.getUser();
-			long goodsId = mm.getGoodsId();
-			
-			GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
-	    	int stock = goods.getStockCount();
-	    	if(stock <= 0) {
-	    		return;
-	    	}
-	    	//判断是否已经秒杀到了
-	    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
-	    	if(order != null) {
-	    		return;
-	    	}
-	    	//减库存 下订单 写入秒杀订单
-	    	miaoshaService.miaosha(user, goods);
-		}
+    @Autowired
+    GoodsService goodsService;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    MiaoshaService miaoshaService;
+
+    @RabbitListener(queues = MQConfig.MIAOSHA_QUEUE)
+    public void receive(String message, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag,com.rabbitmq.client.Channel channel) {
+        log.info("receive message:" + message);
+        MiaoshaMessage mm = RedisService.stringToBean(message, MiaoshaMessage.class);
+        MiaoshaUser user = mm.getUser();
+        long goodsId = mm.getGoodsId();
+
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        int stock = goods.getStockCount();
+        if (stock <= 0) {
+            return;
+        }
+        //判断是否已经秒杀到了
+        MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+        if (order != null) {
+            return;
+        }
+        //把mysql的异常捕获 让消息不会重发
+        try {
+            miaoshaService.miaosha(user, goods);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 //		@RabbitListener(queues=MQConfig.QUEUE)
 //		public void receive(String message) {
@@ -70,5 +82,5 @@ public class MQReceiver {
 //			log.info(" header  queue message:"+new String(message));
 //		}
 
-		
+
 }

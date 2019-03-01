647f5ae245fcb61bcca934eb88eb8795ef34ffc5
lijiecheng <1016030044@qq.com>
2019-03-02 01:11:55 +0800

    第七章 订单缓存 永久

--- src/main/java/com/lijiecheng/miaosha/redis/OrderKey.java
+++ src/main/java/com/lijiecheng/miaosha/redis/OrderKey.java
@@ -3,7 +3,7 @@ package com.lijiecheng.miaosha.redis;
 public class OrderKey extends BasePrefix {
 
 	public OrderKey(String prefix) {
-		super(60,prefix);//一分钟失效
+		super(prefix);//一分钟失效
 	}
 	public static OrderKey getMiaoshaOrderByUidGid = new OrderKey("moug");
 }
